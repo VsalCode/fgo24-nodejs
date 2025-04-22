26fb0296ccf48275b7d017c809e4184db191e9d9
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-22 19:35:08 +0700

    Update minitask2.js

--- minitask2.js
+++ minitask2.js
@@ -2,16 +2,22 @@ let fs = require('fs').promises
 const path = require('node:path');
 const url = "https://gist.githubusercontent.com/fulsep/58daee79a699d0fe143da36bbc47e3d6/raw/dcc2e9c970af553011ba6d2d13331e68f5cf42ff/lagu.json"
 
-
-fetch(url).then((res) => {
-  return res.json() 
-}).then((data) => {
-  let listMusic = data.map(item => item.split(" - ").reverse().join(" - "))
-  return listMusic
-}).catch((err) => {
-  console.log(err);
-})
-
+const main = async () => {
+  try{
+    const fetchData = await fetch(url)
+    const data = await fetchData.json()
+    // console.log(data);
+    const listMusic = data.map(item => item.split(" - ").reverse().join(" - "))
+   
+    listMusic.map(item => { 
+      createFile(path.join('music',`${item}.mp3`)) 
+    })
+ 
+    console.log("Music berhasil dipindahkan");
+  }catch(err){
+    console.log(err);
+  }
+}
 
 async function createFile(fileName){
   try {
@@ -23,7 +29,4 @@ async function createFile(fileName){
   }
 }
 
-listMusic.forEach(music => {
-  createFile(path.join("music",`${music}.mp3`))
-})
-
+main()
